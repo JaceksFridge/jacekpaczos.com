9c1c8512d6d0b3a74a497f4180aeda72daeb3c26
JaceksFridge <Japacz22@gmail.com>
2023-07-28 20:03:56 +0200

    animated bg

--- src/pages/CaseReact20.js
+++ src/pages/CaseReact20.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from 'react';
+
 
 const CaseReact20 = () => {
+
+  const redVideo = './assets/videos/red-rotate.mp4'
+
   return (
     <div className="module">
-        <div className="r10 light">10%</div>
-        <div className="r10 black">20%</div>
-        <div className="r10 light">30%</div>
-        <div className="r10 black">40%</div>
-        <div className="r10 light">50%</div>
-        <div className="r10 black">60%</div>
-        <div className="r10 light">70%</div>
-        <div className="r10 black">80%</div>
-        <div className="r10 light">90%</div>
-        <div className="r10 black">100%</div>        
+      <video 
+        className="red-video"
+        autoPlay="autoplay"
+        loop="loop"
+        muted
+        src={redVideo}
+      >
+      </video>
+      <div className="overlay">
+        <h1 className="text">Welcome to the Future</h1>
+      </div>
+     
     </div>
   )
 }
 
-export default CaseReact20
+export default CaseReact20;
