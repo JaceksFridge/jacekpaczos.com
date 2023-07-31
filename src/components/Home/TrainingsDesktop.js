b846d91436e00d9a568ef9e0ade1ce3f503b3868
JaceksFridge <Japacz22@gmail.com>
2023-08-01 00:19:53 +0200

    added trainings card

--- src/components/Home/TrainingsDesktop.js
+++ src/components/Home/TrainingsDesktop.js
@@ -1,21 +1,22 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom';
 
-const TrainingsDesktop = ({trainings }) => {
+import CardDesktop from '../CardDesktop'
+
+const TrainingsDesktop = ({ trainings }) => {
   return (
 
-    <div className="projects-desk">
-      <div className="projects-desk-container">
+    <div className="trainings-desk">
+      <div className="trainings-desk-container">
         {trainings.map((training, index) => (
-              <div className="project-desk-sec">
-                  <div className="project-desk-text">
-                      <h4 className="project-desk-title">{training.title}</h4>
-                      <h3 className="project-desk-large">{training.text}</h3>
-                      <Link to={`/$training.link}`} class="project-link">open</Link>
+              <div className="training-desk-sec" key={index}>
+                  <div className="training-desk-text">
+                      <h4 className="training-desk-title">{training.title}</h4>
+                      <h3 className="training-desk-large">{training.text}</h3>
+                      <Link to={training.link} className="training-link">open</Link>
                   </div>
-                  <div className="project-desk-image">
-
+                  <div className="training-desk-image">
+                    <CardDesktop key={index} data={training}/>
                   </div>
               </div>
           ))}
