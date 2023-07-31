1b0ff227364fe5fd5c4f129df7d95f6fe6689cc2
JaceksFridge <Japacz22@gmail.com>
2023-07-31 18:34:21 +0200

    case hero

--- src/pages/CaseReact20.js
+++ src/pages/CaseReact20.js
@@ -1,24 +1,31 @@
+
+
+
 import React from 'react';
 
 
 const CaseReact20 = () => {
 
-  const redVideo = './assets/videos/red-rotate.mp4'
-
   return (
-    <div className="module">
-      <video 
-        className="red-video"
-        autoPlay="autoplay"
-        loop="loop"
-        muted
-        src={redVideo}
-      >
-      </video>
-      <div className="overlay">
-        <h1 className="text">Welcome to the Future</h1>
+    <div className="case">
+      <div className="case-background"></div>
+      <div className="case-info">
+        <h1 className="case-title">Moonlog.io</h1>
+        <div className="case-boxes">
+          <div className="case-box">
+            <h3 className="case-box-key">/type</h3>
+            <h5 className="case-box-value">Building</h5>
+          </div>
+          <div className="case-box">
+            <h3 className="case-box-key">/time</h3>
+            <h5 className="case-box-value">3 weeks</h5>
+          </div>
+          <div className="case-box">
+            <h3 className="case-box-key">/role</h3>
+            <h6 className="case-box-value">Designer, Developer</h6>
+          </div>
+        </div>
       </div>
-     
     </div>
   )
 }
