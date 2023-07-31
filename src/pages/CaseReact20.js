ee1f1a0eec7e2586260b3ee4988ca150e3f21dca
JaceksFridge <Japacz22@gmail.com>
2023-07-31 19:13:11 +0200

    mockup moonlog

--- src/pages/CaseReact20.js
+++ src/pages/CaseReact20.js
@@ -8,13 +8,15 @@ const CaseReact20 = () => {
 
   return (
     <div className="case">
-      <div className="case-background"></div>
+      <div className="case-background">
+        <img src="./assets/images/moonlog/moonglog-mockup.png" alt="moonlog" className="case-background-image" />
+      </div>
       <div className="case-info">
         <h1 className="case-title">Moonlog.io</h1>
         <div className="case-boxes">
           <div className="case-box">
             <h3 className="case-box-key">/type</h3>
-            <h5 className="case-box-value">Building</h5>
+            <h5 className="case-box-value">Personal Webapp</h5>
           </div>
           <div className="case-box">
             <h3 className="case-box-key">/time</h3>
@@ -22,7 +24,7 @@ const CaseReact20 = () => {
           </div>
           <div className="case-box">
             <h3 className="case-box-key">/role</h3>
-            <h6 className="case-box-value">Designer, Developer</h6>
+            <h6 className="case-box-value">Designer and Developer</h6>
           </div>
         </div>
       </div>
