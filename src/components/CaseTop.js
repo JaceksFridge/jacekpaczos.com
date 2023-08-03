c67d4e0fb6b7990814f31848676d0d2a7348385a
JaceksFridge <Japacz22@gmail.com>
2023-08-03 16:23:54 +0200

    reduced video file size

--- src/components/CaseTop.js
+++ src/components/CaseTop.js
@@ -16,6 +16,7 @@ const CaseTop = ({ casestudy }) => {
                 <video 
                     src={casestudy.vid} 
                     autoPlay 
+                    playsInline
                     loop 
                     muted 
                     style={{width: "100%", height: "100%"}}
