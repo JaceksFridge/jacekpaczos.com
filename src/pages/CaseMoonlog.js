c3b5ec03e679a3ce109585d5e398b83300f84eb7
JaceksFridge <Japacz22@gmail.com>
2023-10-22 01:54:47 +0200

    adding text

--- src/pages/CaseMoonlog.js
+++ src/pages/CaseMoonlog.js
@@ -14,7 +14,7 @@ const CaseMoonlog = () => {
     query: '(min-device-width: 1224px)'
   })
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState(null)
    
   useEffect(() => {
     const fetchData = async () => {
@@ -27,44 +27,47 @@ const CaseMoonlog = () => {
 
   return (
     <>
-      {/* { isDesktoporLaptop ? (
-        <CaseTopDesktop casestudy={data} />
-      ) : (
-        <CaseTop casestudy={data} />
-      )} */}
-      <Navbar />
-      <div className="case-hero">
-        <img
-          src="/assets/images/moonlog/moonlog_hero.webp" 
-          alt="Moonlog Hero" 
-          loading="lazy"
-        />
-      </div>
-      <div className="case-info">
-        <h1 className="case-title">Gamifying Life</h1>
-        <h3 className="case-subtitle">moonlog is a fullstack react app</h3>
-        <div className="hero-btn-container">
-          <Button 
-            className="btn-github"
-            text="git"
-            url="https://github.com/JaceksFridge"
-            // img="/assets/icons/Moonlog/github.svg"
-            width="3rem"
-          />
-          <Button 
-            className="btn-website"
-            text="web"
-            url="https://jacekpaczos.com"
-            // img="./assets/icons/Moonlog/website.svg"
-            width="3rem"
-          />
+      { !data ? (
+        <div>
+          Loading...
         </div>
-      </div>
-      <div className="case-infoblock">
-        <h2 className="case-section-title">strategy</h2>
-        <div className="section-line"></div>
-        <p className="section-text"></p>
-      </div>
+      ) : (
+        <div className="casestudy">
+          <Navbar />
+          <div className="case-hero">
+            <img
+              src="/assets/images/moonlog/moonlog_hero.webp" 
+              alt="Moonlog Hero" 
+              loading="lazy"
+            />
+          </div>
+          <div className="case-info">
+            <h1 className="case-title">Gamifying Life</h1>
+            <h3 className="case-subtitle">moonlog is a fullstack react app</h3>
+            <div className="hero-btn-container">
+              <Button 
+                className="btn-github"
+                text="git"
+                url="https://github.com/JaceksFridge"
+                // img="/assets/icons/Moonlog/github.svg"
+                width="3rem"
+              />
+              <Button 
+                className="btn-website"
+                text="web"
+                url="https://jacekpaczos.com"
+                // img="./assets/icons/Moonlog/website.svg"
+                width="3rem"
+              />
+            </div>
+          </div>
+          <div className="case-infoblock">
+            <h2 className="case-section-title">strategy</h2>
+            <div className="section-line"></div>
+            <p className="section-text">{data.sections.strategy}</p>
+          </div>
+        </div>
+      )}
     </>
   )
 }
