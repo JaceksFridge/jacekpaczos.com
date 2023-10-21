65bb7101affdba1dcf22785972c26ff0c487c1ca
JaceksFridge <Japacz22@gmail.com>
2023-10-22 01:33:54 +0200

    editing case info block

--- src/pages/CaseMoonlog.js
+++ src/pages/CaseMoonlog.js
@@ -5,6 +5,7 @@ import CaseTopDesktop from '../components/CaseTopDesktop'
 import { useMediaQuery } from 'react-responsive'
 
 import Navbar from "../components/Navbar"
+import Button from "../components/Button"
 
 
 const CaseMoonlog = () => {
@@ -32,6 +33,36 @@ const CaseMoonlog = () => {
         <CaseTop casestudy={data} />
       )} */}
       <Navbar />
+      <div className="case-hero">
+        <img
+          src="/assets/images/moonlog/moonlog_hero.webp" 
+          alt="Moonlog Hero" 
+          loading="lazy"
+        />
+      </div>
+      <div className="case-info">
+        <h1 className="case-title">Gamifying Life</h1>
+        <h2 className="case-subtitle">moonlog is a fullstack react app</h2>
+        <div className="hero-btn-container">
+          <Button 
+            className="btn-github"
+            text="git"
+            url="https://github.com/JaceksFridge"
+            // img="/assets/icons/Moonlog/github.svg"
+            width="3rem"
+          />
+          <Button 
+            className="btn-website"
+            text="web"
+            url="https://jacekpaczos.com"
+            // img="./assets/icons/Moonlog/website.svg"
+            width="3rem"
+          />
+        </div>
+      </div>
+      <div className="case-infoblock">
+        
+      </div>
     </>
   )
 }
