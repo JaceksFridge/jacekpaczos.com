bca7704177d075945479cc92af7c95d20cf58888
JaceksFridge <Japacz22@gmail.com>
2023-10-22 00:47:31 +0200

    editing case

--- src/pages/CaseMoonlog.js
+++ src/pages/CaseMoonlog.js
@@ -4,31 +4,34 @@ import CaseTop from '../components/CaseTop';
 import CaseTopDesktop from '../components/CaseTopDesktop'
 import { useMediaQuery } from 'react-responsive'
 
+import Navbar from "../components/Navbar"
+
 
 const CaseMoonlog = () => {
 
-  const isDesktoporLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)'
-  })
+  // const isDesktoporLaptop = useMediaQuery({
+  //   query: '(min-device-width: 1224px)'
+  // })
 
-  const [data, setData] = useState([])
+  // const [data, setData] = useState([])
    
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('./data/CaseMoonlog.json')
-      const jsonData = await response.json()
-      setData(jsonData.hero)
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch('./data/CaseMoonlog.json')
+  //     const jsonData = await response.json()
+  //     setData(jsonData.hero)
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
     <>
-      { isDesktoporLaptop ? (
+      {/* { isDesktoporLaptop ? (
         <CaseTopDesktop casestudy={data} />
       ) : (
         <CaseTop casestudy={data} />
-      )}
+      )} */}
+      <Navbar />
     </>
   )
 }
